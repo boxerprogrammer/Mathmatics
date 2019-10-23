d56ff0c0f7e84b14b29fb43c019f16b81446c1c1
Ryuichi Kawano <r_kawano@asojuku.ac.jp>
2019-10-23 21:01:03 +0900

    CCD-IKについて実装

--- CCD-IK/CCD-IK/Geometry.h
+++ CCD-IK/CCD-IK/Geometry.h
@@ -34,7 +34,11 @@ struct Vector2D {
 	float SQLength()const {
 		return x*x+y*y;
 	}
-
+	void Normalize() {
+		auto len = Length();
+		x /= len;
+		y /= len;
+	}
 	Vector2D<float> Normalized()const {
 		auto len = Length();
 		return Vector2D<float>((float)x / len, (float)y / len);
@@ -85,3 +89,30 @@ typedef Vector2f Position2f;
 Vector2f ConvertToVector2f(const Vector2& v);
 
 float Dot(const Vector2f& lval, const Vector2f& rval);
+float Cross(const Vector2f& lval, const Vector2f& rval);
+struct Matrix {
+	float m[3][3];
+};
+
+///単位行列を返す
+Matrix IdentityMat();
+
+///平行移動行列を返す
+///@param x X方向平行移動量
+///@param y Y方向平行移動量
+Matrix TranslateMat(float x, float y);
+
+///回転行列を返す
+///@param angle 回転角度
+Matrix RotateMat(float angle);
+
+///２つの行列の乗算を返す
+///@param lmat 左辺値(行列)
+///@param rmat 右辺値(行列)
+///@attention 乗算の順序に注意してください
+Matrix MultipleMat(const Matrix& lmat, const Matrix& rmat);
+
+///ベクトルに対して行列乗算を適用し、結果のベクトルを返す
+///@param mat 行列
+///@param vec ベクトル
+Vector2f MultipleVec(const Matrix& mat, const Vector2f& vec);
--- CCD-IK/CCD-IK/main.cpp
+++ CCD-IK/CCD-IK/main.cpp
@@ -1,5 +1,8 @@
 #include<DxLib.h>
 #include<vector>
+#include<array>
+#include<algorithm>
+#include<numeric>
 #include"Geometry.h"
 
 using namespace std;
@@ -10,9 +13,205 @@ float Clampf(float value, float minV = 0.0f, float maxV = 1.0f) {
 Position2f Clamp(Position2f value, Position2f minV = Position2f(0.0f, 0.0f), Position2f maxV = Position2f(1.0f, 1.0f)) {
 	return Position2f(Clampf(value.x, minV.x, maxV.x), Clampf(value.y, minV.y, maxV.y));
 }
+
+constexpr uint32_t points_num = 4;
+
+void Draw(int rt, int screen_width, int screen_height, 
+	vector<Position2f> &positions, Position2f& target) {
+	SetDrawScreen(rt);
+	DrawBox(0, 0, screen_width, screen_height, 0xffffff, true);
+
+	//影表示
+	DrawCircleAA(target.x + 2,
+		target.y + 2,
+		25, 32,
+		0x000000,
+		false,
+		2);
+	for (int i = 0; i < positions.size(); ++i) {
+
+		DrawCircleAA(positions[i].x + 2,
+			positions[i].y + 2,
+			25, 32,
+			0x000000,
+			false,
+			2);
+		if (i < positions.size() - 1) {
+			DrawLineAA(positions[i].x + 2,
+				positions[i].y + 2,
+				positions[i + 1].x + 2,
+				positions[i + 1].y + 2,
+				0x000000, 5);
+		}
+	}
+	GraphFilter(rt, DX_GRAPH_FILTER_GAUSS, 4, 800);
+	SetDrawScreen(DX_SCREEN_BACK);
+	DrawGraph(0, 0, rt, true);
+
+	//通常表示
+	for (int i = 0; i < positions.size(); ++i) {
+
+		DrawCircleAA(positions[i].x,
+			positions[i].y,
+			25, 32,
+			0x000000,
+			false,
+			2);
+		if (i < positions.size() - 1) {
+			DrawLineAA(positions[i].x,
+				positions[i].y,
+				positions[i + 1].x,
+				positions[i + 1].y,
+				0x000000, 5);
+		}
+		DrawCircleAA(target.x,
+			target.y,
+			25, 32,
+			0xaa0000,
+			false,
+			2);
+	}
+
+}
+
+void MoveControlPoint(vector<Position2f> &positions, 
+	array<bool, points_num> &overlapped, 
+	int &lastMouseInput, 
+	bool &captured, 
+	int &capturedNo, 
+	std::vector<float> &edgeLens) {
+	//端点ドラッグアンドドロップコントロール
+	int mx, my;
+	GetMousePoint(&mx, &my);
+	Position2f mp(mx, my);
+	for (int i = 0; i < positions.size(); ++i) {
+		auto diff = mp - positions[i];
+		auto r = diff.Length();
+		if (r <= 20.0f&&i == points_num - 1) {//端点以外は選択しない
+			overlapped[i] = true;
+		}
+		else {
+			overlapped[i] = false;
+		}
+	}
+	auto currentMouseInput = GetMouseInput();
+	if (currentMouseInput) {
+		if (lastMouseInput == 0) {
+			for (int i = 0; i < positions.size(); ++i) {
+				auto diff = mp - positions[i];
+				auto r = diff.Length();
+				if (r <= 20.0f&&i == points_num - 1) {//端点のみ選択可能
+					captured = true;
+					capturedNo = i;
+				}
+			}
+		}
+		else {
+			if (captured&&capturedNo >= 0) {
+				positions[capturedNo] = Clamp(mp, Position2f(0, 0), Position2f(640, 480));
+				//もちろん、骨の長さ以上に離れようと、してはいけない(戒め)
+				auto linearVec = positions.back() - positions.front();
+				auto limitLen = std::accumulate(edgeLens.begin(), edgeLens.end(), 0);
+
+				if (linearVec.Length() >= limitLen) {
+					linearVec = linearVec.Normalized()*limitLen;
+					if (capturedNo = points_num - 1) {
+						positions[capturedNo] = positions[0] + linearVec;
+					}
+				}
+
+			}
+		}
+		lastMouseInput = currentMouseInput;
+	}
+	else {
+		lastMouseInput = 0;
+		capturedNo = -1;
+		captured = false;
+	}
+}
+
+void MoveTarget(Position2f& target, int &lastMouseInput, bool &captured, bool& targetOverlapped){
+	//端点ドラッグアンドドロップコントロール
+	int mx, my;
+	GetMousePoint(&mx, &my);
+	Position2f mp(mx, my);
+	auto diff = mp - target;
+	auto r = diff.Length();
+	if (r <= 20.0f) {//端点以外は選択しない
+		targetOverlapped = true;
+	}
+	else {
+		targetOverlapped = false;
+	}
+	
+	auto currentMouseInput = GetMouseInput();
+	if (currentMouseInput) {
+		if (lastMouseInput == 0) {
+			auto diff = mp - target;
+			auto r = diff.Length();
+			if (r <= 20.0f) {//端点のみ選択可能
+				captured = true;
+			}
+		}
+		else {
+			if (captured) {
+				target = Clamp(mp, Position2f(0, 0), Position2f(640, 480));
+				//もちろん、骨の長さ以上に離れようと、してはいけない(戒め)
+				
+
+			}
+		}
+		lastMouseInput = currentMouseInput;
+	}
+	else {
+		lastMouseInput = 0;
+		captured = false;
+	}
+}
+
+///positionsの末端がtargetに一致し、かつ先頭を動かさないようにpositionsを書き換える
+void CCD_IK(Position2f target, vector<Position2f>& positions, std::vector<float>& edgeLens,unsigned int cyclic = 4) {
+	//もしtargetとpositionsの先頭が全骨の長さより長いなら角度は180確定なのでCCD-IKを行わず末端から回転させる
+	if (accumulate(edgeLens.begin(), edgeLens.end(), 0) < (target - positions.front()).Length()) {
+		auto vec = target - positions.front();
+		vec.Normalize();
+		for (int i = 0; i < edgeLens.size(); ++i) {
+			positions[i + 1] = positions[i] + vec*edgeLens[i];
+		}
+		return;
+	}
+
+	//繰り返し回数だけ繰り返す
+	for (int c = 0; c < cyclic; ++c) {
+		//末端から攻めていく
+		auto rit = positions.rbegin();
+		++rit;//末端との角度で計算するため、末端は省く
+		for (;rit!= positions.rend(); ++rit) {
+			//回転角度計算
+			Vector2f vecToEnd = positions.back() - *rit;//末端と現在のノードのベクトル
+			Vector2f vecToTarget = target - *rit;//現在のノードからターゲット(エフェクタ)へのベクトル
+			vecToEnd.Normalize();
+			vecToTarget.Normalize();
+			//2Dだから角度計算は単純(3Dの場合こうはいかない)
+			auto cross = Cross(vecToEnd, vecToTarget);
+			auto dot = Dot(vecToEnd, vecToTarget);
+			auto angle = atan2f(Cross(vecToEnd, vecToTarget), Dot(vecToEnd, vecToTarget));
+			//回転行列を得る
+			Matrix mat = MultipleMat(TranslateMat(rit->x,rit->y),MultipleMat(RotateMat(angle),TranslateMat(-rit->x,-rit->y)));
+			//現在のノードから末端まで回転する
+			auto it = rit.base();
+			for (; it != positions.end(); ++it) {
+				*it = MultipleVec(mat, *it);
+			}
+
+
+		}
+	}
+}
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	ChangeWindowMode(true);
-	SetWindowText("余弦定理IK");
+	SetWindowText("CCD-IK 2D");
 	ChangeWindowMode(true);
 	if (DxLib_Init() == -1) {
 		return -1;
@@ -23,17 +222,18 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	auto rt = MakeScreen(screen_width, screen_height);
 
 
-
+	bool targetOverlapped = false;
 	int currentX = 512;
 	int capturedNo = -1;
 	int lastMouseInput = 0;
 	bool captured = false;
 	char keystate[256];
 	char lastKeyState[256];
-	bool overlapped[3] = {};
-	vector<Position2f> positions(3);
+	
+	array<bool, points_num> overlapped = {};
+	vector<Position2f> positions(points_num);
 	for (int i = 0; i < positions.size(); ++i) {
-		positions[i] = Position2f(50 + i * 250, 240);
+		positions[i] = Position2f(50 + i * (500/(points_num-1)), 240);
 	}
 	std::vector<float> edgeLens(positions.size() - 1);
 	for (int i = 0; i < edgeLens.size(); ++i) {
@@ -41,132 +241,21 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	}
 
 	float angle = 0.0f;
+	Position2f target = positions.back();
 	while (ProcessMessage() == 0) {
-		SetDrawScreen(rt);
-		DrawBox(0, 0, screen_width, screen_height, 0xffffff, true);
 
-		positions[2] = Position2f(425 + cos(angle) * 120, 240 - sin(angle) * 120);
-		angle += 0.05f;
+		//コントロールポイントの移動
+		//MoveControlPoint(positions, overlapped, lastMouseInput, captured, capturedNo, edgeLens);
+		MoveTarget(target, lastMouseInput, captured, targetOverlapped);
 
-		int mx, my;
-		GetMousePoint(&mx, &my);
-		Position2f mp(mx, my);
-
-		for (int i = 0; i < positions.size(); ++i) {
-			auto diff = mp - positions[i];
-			auto r = diff.Length();
-			if (r <= 20.0f&&i != 1) {//中間地点は選択しない
-				overlapped[i] = true;
-			}
-			else {
-				overlapped[i] = false;
-			}
-		}
+		CCD_IK(target, positions,edgeLens);
 
 
-		auto currentMouseInput = GetMouseInput();
-		if (currentMouseInput) {
-			if (lastMouseInput == 0) {
-				for (int i = 0; i < positions.size(); ++i) {
-					auto diff = mp - positions[i];
-					auto r = diff.Length();
-					if (r <= 20.0f&&i != 1) {//中間地点は選択しない
-						captured = true;
-						capturedNo = i;
-					}
-				}
-			}
-			else {
-				if (captured&&capturedNo >= 0) {
-					positions[capturedNo] = Clamp(mp, Position2f(0, 0), Position2f(640, 480));
-					//もちろん、骨の長さ以上に離れようと、してはいけない(戒め)
-					auto linearVec = positions[2] - positions[0];
-					auto limitLen = edgeLens[0] + edgeLens[1];
-					if (linearVec.Length() >= limitLen) {
-						linearVec = linearVec.Normalized()*limitLen;
-						if (capturedNo == 0) {
-							positions[0] = positions[2] - linearVec;
-						}
-						else {
-							positions[2] = positions[0] + linearVec;
-						}
-					}
-
-				}
-			}
-			lastMouseInput = currentMouseInput;
-		}
-		else {
-			lastMouseInput = 0;
-			capturedNo = -1;
-			captured = false;
-		}
-
-		//position[0]とposition[2]の位置関係からposition[1]を求める
-		Vector2f linearVec = positions[2] - positions[0];
-		float A = linearVec.Length();
-		float B = edgeLens[0];
-		float C = edgeLens[1];
-		linearVec = linearVec.Normalized();
-		float angleFromX = atan2(linearVec.y, linearVec.x);
-		float theta = acosf((A*A + B * B - C * C) / (2 * A*B));
-		angleFromX += theta;
-
-		if (A < B + C) {
-			positions[1] = positions[0] + Vector2f(cosf(angleFromX), sinf(angleFromX))*edgeLens[0];
-		}
-		else {
-			positions[1] = positions[0] + linearVec * edgeLens[0];
-		}
-
-		//影表示
-		for (int i = 0; i < positions.size(); ++i) {
-
-			DrawCircleAA(positions[i].x + 2,
-				positions[i].y + 2,
-				25, 32,
-				0x000000,
-				false,
-				2);
-			if (i < positions.size() - 1) {
-				DrawLineAA(positions[i].x + 2,
-					positions[i].y + 2,
-					positions[i + 1].x + 2,
-					positions[i + 1].y + 2,
-					0x000000, 5);
-			}
-		}
-		GraphFilter(rt, DX_GRAPH_FILTER_GAUSS, 4, 800);
-		SetDrawScreen(DX_SCREEN_BACK);
-		DrawGraph(0, 0, rt, true);
-		//通常表示
-
-		DrawCircleAA(425, 240, 120, 32, 0x00aa00, false, 1);
-
-		for (int i = 0; i < positions.size(); ++i) {
-			uint32_t col = 0x000000;
-			if (overlapped[i]) {
-				col = 0x0000aa;
-			}
-			if (i == capturedNo) {
-				col = 0xaa0000;
-			}
-			DrawCircleAA(positions[i].x,
-				positions[i].y,
-				25, 32,
-				col,
-				false,
-				2);
-			if (i < positions.size() - 1) {
-				DrawLineAA(positions[i].x,
-					positions[i].y,
-					positions[i + 1].x,
-					positions[i + 1].y,
-					0x000000, 5);
-			}
-		}
+		//描画
+		Draw(rt, screen_width, screen_height, positions, target);
 
 		ScreenFlip();
+
 	}
 	DxLib_End();
 }
